5d773795af416e658a2211e2236b6bee8b7f5d18
Anna Shevchenko <uaskoa@gmail.com>
2020-12-05 16:03:11 +0200

    task-07

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,61 @@
 let input;
-const numbers = [];
+let numbers = [];
 let total = 0;
 
+while (true) {
+  input = prompt("Введите число");
+  if (input === null) {
+    break;
+  }
+  input = Number(input);
 
+  if (Number.isNaN(input)) {
+    alert("Было введено не число, попробуйте еще раз");
+    continue;
+  }
+
+  numbers.push(input);
+}
+
+for (const number of numbers) {
+  total += number;
+}
+
+if (numbers.length !== 0) {
+  console.log(`Общая сумма чисел равна ${total}.`);
+}
+
+
+// С ИСПОЛЬЗОВАНИЕМ ФУНКЦИИ
+// const sum = function() { 
+//   for (const number of numbers) {
+//   total += number;
+// }
+
+// if (numbers.length !== 0) {
+//   return `Общая сумма чисел равна ${total}.`;
+// }
+// }
+
+// console.log(sum(numbers));
+
+
+
+// do {
+//   input = prompt("Введите число");
+//   if (input === null) {
+//     break;
+//   }
+//   input = Number(input);
+//   const inputNotANumber = Number.isNaN(input);
+//     if (inputNotANumber) {
+//     alert("Было введено не число, попробуйте еще раз");
+//     continue;
+//   }
+
+//   numbers.push(input);
+
+// } while (input !== null);
 
 // Напиши скрипт со следующим функционалом:
 
@@ -23,3 +76,11 @@ let total = 0;
 // let input;
 // const numbers = [];
 // let total = 0;
+
+//  input = Number(input);
+//  const inputNotANumber = Number.isNaN(input);
+
+//  if (inputNotANumber) {
+//    alert("Было введено не число, попробуйте еще раз");
+//    continue;
+//  }
